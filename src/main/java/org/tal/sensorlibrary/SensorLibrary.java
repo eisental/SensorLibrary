616d13172eeda1dc022540bd7d546313ec01dc81
Tal Eisenberg <eisental@gmail.com>
2011-03-10 00:36:02 +0100

    Updated to work with RC0.83 new circuit library loading mechanism

--- src/main/java/org/tal/sensorlibrary/SensorLibrary.java
+++ src/main/java/org/tal/sensorlibrary/SensorLibrary.java
@@ -5,33 +5,15 @@
 
 package org.tal.sensorlibrary;
 
-import java.util.logging.Logger;
-import org.bukkit.plugin.java.JavaPlugin;
-import org.tal.redstonechips.RedstoneChips;
 import org.tal.redstonechips.circuit.CircuitLibrary;
 
 /**
  *
  * @author Tal Eisenberg
  */
-public class SensorLibrary extends JavaPlugin {
-    public static final Logger logger = Logger.getLogger("Minecraft");
-
-    public SensorLibrary() {
-        new CircuitLibrary() {
-            @Override
-            public Class[] getCircuitClasses() {
-                return new Class[] {photocell.class, pirsensor.class, rangefinder.class, daytime.class};
-            }
-        };
-    }
-
+public class SensorLibrary extends CircuitLibrary {
     @Override
-    public void onDisable() {
+    public Class[] getCircuitClasses() {
+        return new Class[] {photocell.class, pirsensor.class, rangefinder.class, daytime.class};
     }
-
-    @Override
-    public void onEnable() {
-    }
-
 }
